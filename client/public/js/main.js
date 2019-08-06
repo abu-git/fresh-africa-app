61a76312bedfa002d3578875b746d49b0575cf5d
abu-git <abu.sammie@gmail.com>
2019-08-06 02:51:47 +0200

    updates

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -59,7 +59,7 @@ var swiper = new Swiper('.team-slider', {
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker
-            .register('../sw.js')
+            .register('/sw.js')
             .then(reg => console.log('Service Worker: Registered (Pages)'))
             .catch(err => console.log(`Service Worker: Error: ${err}`));
     });
